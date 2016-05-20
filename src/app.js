0060707aa4b4e77e22530319c39c56cd6528962e
Paul Diaz Navarrete <paulrrdiaz@gmail.com>
2016-05-20 15:23:53 -0500

    Add first component

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+const cards = (state, action) => {
+  switch (action.type) {
+    case 'ADD_CARD':
+      let newCard = Object.assign({}, action.data, {
+        score: 1,
+        id: +new Date
+      });
+
+      return state.concat([newCard]);
+    default:
+      return state || [];
+  }
+}
+const store = Redux.createStore(Redux.combineReducers({
+  cards
+}));
+
+const App = (props) => {
+  return (
+    <div class='app'>
+      <h1>Hello World :3</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
