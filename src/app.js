9358808b533cd1c7951c1d44fafeb2931d2b0f5b
Paul Diaz Navarrete <paulrrdiaz@gmail.com>
2016-06-02 18:52:43 -0500

    Add react-router and react-router-redux

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Redux from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import * as reducers from './reducers';
+reducers.routing = routerReducer;
 import App from './components/App';
 import Sidebar from './components/Sidebar';
 
-const store = Redux.createStore(Redux.combineReducers(reducers));
+const store = createStore(combineReducers(reducers));
+const history = syncHistoryWithStore(browserHistory, store);
+
+/*
+  This line help to solve 
+  const routes = (<Route path='/' component={App}></Route>)
+*/
 
 function run() {
   let state = store.getState();
-  console.log(state);
   ReactDOM.render((
     <Provider store={store}>
-      <App>
-        <Sidebar />                
-      </App>
+      <Router history={history}>
+        <Route path='/' component={App}></Route>
+      </Router>
     </Provider>
   ), document.getElementById('root'));
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Sidebar from './Sidebar';
 
 const App = (props) => {
   return (
     <div className='app'>
-      {props.children}
+      <Sidebar />
     </div>
   )
 }
